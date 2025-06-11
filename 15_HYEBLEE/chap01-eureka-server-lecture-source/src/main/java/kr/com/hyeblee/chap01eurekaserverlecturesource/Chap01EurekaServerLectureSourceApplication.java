d2b4438a4b5f685107dd11140507acc1f9e6c7a1
hyeblee <hyeb1224@naver.com>
2025-06-11 12:22:31 +0900

    2025-06-11

---
+++ 15_HYEBLEE/chap01-eureka-server-lecture-source/src/main/java/kr/com/hyeblee/chap01eurekaserverlecturesource/Chap01EurekaServerLectureSourceApplication.java
@@ -0,0 +1,16 @@
+package kr.com.hyeblee.chap01eurekaserverlecturesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// 해당 어플리케이션을 Eureka Server로 활성화
+@EnableEurekaServer
+public class Chap01EurekaServerLectureSourceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Chap01EurekaServerLectureSourceApplication.class, args);
+  }
+
+}
---
+++ 15_HYEBLEE/chap01-eureka-server-lecture-source/src/test/java/kr/com/hyeblee/chap01eurekaserverlecturesource/Chap01EurekaServerLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.com.hyeblee.chap01eurekaserverlecturesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01EurekaServerLectureSourceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
